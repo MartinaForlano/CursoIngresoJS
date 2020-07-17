b2f5a056d1f5741fd42ab3514c30d1edd489dbcf
MartinaForlano1 <martinaforlano44@gmail.com>
2020-07-17 04:13:03 -0300

    TP 1 Ferrete Facturación

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,67 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var suma;
+
+	primerPrecio = txtIdPrecioUno.value;
+	primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = txtIdPrecioDos.value;
+	segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = txtIdPrecioTres.value;
+	tercerPrecio = parseInt (tercerPrecio);
+
+	suma = (primerPrecio + segundoPrecio + tercerPrecio);
+	alert("La suma es de: " +suma);
 }
 function Promedio () 
 {
-	
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var suma;
+	var promedio;
+
+	primerPrecio = txtIdPrecioUno.value;
+	primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = txtIdPrecioDos.value;
+	segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = txtIdPrecioTres.value;
+	tercerPrecio = parseInt (tercerPrecio);
+
+	suma = (primerPrecio + segundoPrecio + tercerPrecio);
+
+	promedio = suma/3;
+
+	alert("El promedio es de: " + promedio);	
 }
+
 function PrecioFinal () 
 {
-	
+	var primerPrecio;
+	var segundoPrecio;
+	var tercerPrecio;
+	var suma;
+	var iva;
+
+	primerPrecio = txtIdPrecioUno.value;
+	primerPrecio = parseInt (primerPrecio);
+
+	segundoPrecio = txtIdPrecioDos.value;
+	segundoPrecio = parseInt (segundoPrecio);
+
+	tercerPrecio = txtIdPrecioTres.value;
+	tercerPrecio = parseInt (tercerPrecio);
+
+	suma = (primerPrecio + segundoPrecio + tercerPrecio);
+
+	iva = (suma * 0.21) + suma;
+
+	alert("El precio final con IVA es de: " + iva);
 }
